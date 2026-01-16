a1a12ec77f35880fc276213cec9d588e7ca13fa4
RenderDrop <hello@renderdrop.app>
2026-01-16 13:29:25 +0800

    chore: update beta form URL

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,2 +1,2 @@
-export const BETA_FORM_URL = "https://forms.gle/VAuJq1qbhME2doRi7";
+export const BETA_FORM_URL = "https://forms.gle/Ri4d1d6mAH1HG7DN7";
 export const DEMO_MAILTO = "mailto:hello@renderdrop.app?subject=RenderDrop%20Demo%20Request";
